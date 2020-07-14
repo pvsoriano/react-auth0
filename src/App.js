c9da4ab7393a7ffcba2642719f82b1d657d7b6c9
pvsoriano <pvsoriano@me.com>
2020-07-13 21:45:59 -0400

    added navbar & adjusted .css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { Component }from 'react';
+import { Route } from 'react-router-dom';
+import Home from "./Home";
+import Profile from "./Profile";
+import NavBar from "./NavBar";
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+class App extends Component {
+    render() {
+        return (
+            <>
+                <NavBar />
+                <div className="body">
+                    <Route path="/" exact component={Home} />
+                    <Route path="/profile" component={Profile} />
+                </div>
+            </>
+        );
+    }
 }
 
 export default App;
+
--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Router>
+        <Route component={App} />
+    </Router>,
+    document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
