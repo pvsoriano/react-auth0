d6bdab0a0b130e5d1df21606bca7435d297012ed
pvsoriano <pvsoriano@me.com>
2020-07-14 23:52:47 -0400

    Added callback.js and updated auth.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
-import React, { Component }from 'react';
-import { Route } from 'react-router-dom';
+import React, { Component }from "react";
+import { Route } from "react-router-dom";
 import Home from "./Home";
 import Profile from "./Profile";
 import NavBar from "./NavBar";
+import Auth from "./Auth/Auth";
+import Callback from "./Callback";
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.auth = new Auth(this.props.history);
+    }
     render() {
         return (
             <>
                 <NavBar />
                 <div className="body">
-                    <Route path="/" exact component={Home} />
+                    <Route 
+                        path="/" 
+                        exact 
+                        render={props => <Home auth={this.auth} {...props} />} 
+                    />
+                    <Route
+                        path="/callback"
+                        render={props => <Callback auth={this.auth} {...props} />}
+                    />
                     <Route path="/profile" component={Profile} />
                 </div>
             </>
